efe8448b24e3e4d81bdb4b91f2e6e7847eb64192
Ngoc Pham <tokanizar@gmail.com>
2017-12-02 00:59:22 -0500

    Day 2

---
+++ d02_2.php
@@ -0,0 +1,21 @@
+<?php
+
+$sum = 0;
+$fh = fopen('input_d02_1.txt','r');
+
+while ($line = fgetcsv($fh, 1000, "\t")) {
+    sort($line);
+
+    for ($i = 0; $i < count($line); $i ++) {
+        for ($j = count($line) - 1; $j >= 0; $j--) {
+            if ($line[$j] != $line[$i] && $line[$j] % $line[$i] === 0) {
+                $sum += $line[$j] / $line[$i];
+                break 2;
+            }
+        }
+    }
+}
+
+fclose($fh);
+
+var_dump($sum);
