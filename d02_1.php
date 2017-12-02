bb14d23b78749743d4062b1b3d3b2a08dc043de6
Ngoc Pham <tokanizar@gmail.com>
2017-12-02 00:59:22 -0500

    Day 2

---
+++ d02_1.php
@@ -0,0 +1,14 @@
+<?php
+
+$sum = 0;
+$fh = fopen('input_d02_1.txt','r');
+
+while ($line = fgetcsv($fh, 1000, "\t")) {
+    sort($line);
+
+    $sum += $line[count($line) - 1] - $line[0];
+}
+
+fclose($fh);
+
+var_dump($sum);
