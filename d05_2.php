b07393862213100accca1f90e20975af5eac7135
Ngoc Pham <tokanizar@gmail.com>
2017-12-05 08:25:09 -0500

    Day 5

---
+++ d05_2.php
@@ -0,0 +1,23 @@
+<?php
+
+$arr = array_map(function($a) {
+    return intval($a);
+}, file('input_d05_1.txt', FILE_IGNORE_NEW_LINES));
+
+$index = 0;
+$steps = 0;
+
+while ($index < count($arr)) {
+    $tmp = $arr[$index];
+
+    if ($arr[$index] >= 3) {
+        $arr[$index]--;
+    } else {
+        $arr[$index]++;
+    }
+
+    $index += $tmp;
+    $steps++;
+}
+
+var_dump($steps);
