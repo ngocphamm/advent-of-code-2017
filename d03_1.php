1136568aeb8080727dbf9f48fb670ea1734e37b6
Ngoc Pham <tokanizar@gmail.com>
2017-12-06 07:41:43 -0500

    Better solution for day 3.1

--- d03_1.php
+++ d03_1.php
@@ -1,96 +1,28 @@
 <?php
 
-function find_closest_sqrt(int $number)
-{
-    $sqrt = sqrt($number);
+$to_number = 361527;
+$distance = 0;
+$round_num = 1;
+$round = 0;
 
-    if (intval($sqrt)**2 === $number) return $sqrt;
-
-    return intval($sqrt) + 1;
+while ($round_num < $to_number) {
+    $round++;
+    $round_num += 8*$round;
 }
 
-function travel_sprial_matrix(int $size, int $dest_number)
-{
-    // We need odd size
-    if ($size % 2 === 0) $size += 1;
+$distance += $round;
+$edge = $round*2 + 1;
 
-    $matrix = [];
-    $center = intval(floor($size / 2));
-    $x = $y = $center;
-
-    $count = 1;
-
-    // Start at matrix's center
-    $matrix[$x][$y] = $count++;
-
-    $step_right_up = 1;
-
-    while (1) {
-        $step_left_down = $step_right_up + 1;
-
-        // Go right
-        for ($i = 1; $i <= $step_right_up; $i++) {
-            $y += 1;
-
-            if ($count == $dest_number) return get_manhattan_distance($center, $center, $x, $y);
-
-            $matrix[$x][$y] = $count++;
-
-            // Reach the bottom right of the matrix. We are done!
-            if ($x == ($size - 1) && $y == ($size - 1)) break 2;
-        }
-
-        // Go up
-        for ($i = 1; $i <= $step_right_up; $i++) {
-            $x -= 1;
-
-            if ($count == $dest_number) return get_manhattan_distance($center, $center, $x, $y);
-
-            $matrix[$x][$y] = $count++;
-        }
-
-        // Go left
-        for ($i = 1; $i <= $step_left_down; $i++) {
-            $y -= 1;
-
-            if ($count == $dest_number) return get_manhattan_distance($center, $center, $x, $y);
-
-            $matrix[$x][$y] = $count++;
-        }
-
-        // Go down
-        for ($i = 1; $i <= $step_left_down; $i++) {
-            $x += 1;
-
-            if ($count == $dest_number) return get_manhattan_distance($center, $center, $x, $y);
-
-            $matrix[$x][$y] = $count++;
-        }
-
-        $step_right_up += 2;
-    }
-
-    return $matrix;
+$edge_center = $round_num - $round;
+$edge_distance = abs($to_number - $edge_center);
+while ($edge_distance > $round) {
+    // next edge
+    $edge_center -= $round*2;
+    $edge_distance = abs($to_number - $edge_center);
 }
 
-function print_spiral_matrix(array $matrix)
-{
-    $pad = strlen(count($matrix)**2) + 1;
+$distance += $edge_distance;
 
-    for ($i = 0; $i < count($matrix); $i++) {
-        for ($j = 0; $j < count($matrix); $j++) {
-            echo str_pad($matrix[$i][$j], $pad);
-        }
-
-        echo PHP_EOL;
-    }
-}
-
-function get_manhattan_distance($x1, $y1, $x2, $y2)
-{
-    return abs($x1 - $x2) + abs($y1 - $y2);
-}
-
-$dest = 361527;
-$size = find_closest_sqrt($dest);
-var_dump(travel_sprial_matrix($size, $dest));
+// var_dump($round);
+// var_dump($round_num);
+var_dump($distance);
