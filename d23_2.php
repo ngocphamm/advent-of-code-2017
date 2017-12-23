49dbaa2906f0531d36785b049c3286ea4c188fdc
Ngoc Pham <tokanizar@gmail.com>
2017-12-23 10:39:44 -0500

    Add explaination for Day 23 part 2

--- d23_2.php
+++ d23_2.php
@@ -21,14 +21,14 @@ function is_prime($number)
     return true;
 }
 
-$vals = [];
-
-// Instructions
-$ins = file('input_d23.txt', FILE_IGNORE_NEW_LINES);
-
-$i = 0;
-$steps = 0;
-
+// So... This is totally a trick. And I have not yet come up with a generic solution
+// for all cases (by reading the input and analyze it)
+// In this particular case, the interesting part will be that the value of h will be
+// "considered" only 1001 times, from the value of b to the value of c, while
+// c = b + 17000, with the step of 17 (line "sub b -17").
+// Each time, the value of h is set only when f = 0, and that only happens when
+// b = e * d. e and d both increment by 1 in the inner loops, so this becomes when
+// b is NOT a prime number! That's it.
 $count = 0;
 for ($b = 106500; $b <= 123500; $b += 17) {
 
